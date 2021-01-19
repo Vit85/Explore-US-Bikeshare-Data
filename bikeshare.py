06f43126e7b7e41123314d9bf8a7208c449da7d4
Vitali Erdlej <vitali.erdlej@gmx.de>
2021-01-19 21:42:05 +0100

    Add print to bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -190,6 +190,7 @@ def display_raw_data(df):
 
 def main():
     while True:
+        print("This is the main function!")
         city, month, day = get_filters()
         df = load_data(city, month, day)
 
