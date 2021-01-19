d83d095b4ccfb80331cdb62a285bd97c2fc0de8e
Vitali Erdlej <vitali.erdlej@gmx.de>
2021-01-19 21:26:59 +0100

    Changed print in bikeshare.py

--- bikeshare.py
+++ bikeshare.py
@@ -199,7 +199,7 @@ def main():
         user_stats(df)
         display_raw_data(df)
 
-        restart = input("\nWould you like to restart? Enter yes or no.\n")
+        restart = input("\nWould you like to explore more US bikeshare data? Enter yes or no!\n")
         if restart.lower() != 'yes':
             break
 
